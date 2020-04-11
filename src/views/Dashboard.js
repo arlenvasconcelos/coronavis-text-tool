e1975ee0dc9dab780c823327c4308bd8295e1ab8
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-04-11 00:39:22 -0300

    Add PaperCard Component and style

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -3,74 +3,44 @@ import {Container, Row, Col, Form, Button, Card} from 'react-bootstrap'
 import ColumnChart from '../components/ColumnChart'
 import Header from '../components/Header'
 import Search from '../components/Search'
+import PaperCard from '../components/PaperCard'
 
 export default function Dashboard () {
   return (
     <>
-      <div>
-        <Container>
-          <Header/>
-          <Search/>
-          <Row>
-            <Col md="8">
-              <Card style={{margin: '10px 0'}}>
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card style={{margin: '10px 0'}}>
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                    <ColumnChart/>
-                </Card.Body>
-              </Card>
-              <Card style={{margin: '10px 0'}}>
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              
-            </Col>
-            <Col md="4">
-              <Card style={{margin: '10px 0'}}>
-                <Card.Body>
-                  <Card.Title>Paper 1</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card style={{margin: '10px 0'}}>
-                <Card.Body>
-                  <Card.Title>Paper 2</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card style={{margin: '10px 0'}}>
-                <Card.Body>
-                  <Card.Title>Paper 3</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            
-          </Row>
-        </Container>
-      </div>
+      <Container>
+        <Header/>
+        <Search/>
+        <Row>
+          <Col md="8">
+            <Card className="card">
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card className="card">
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                  <ColumnChart/>
+              </Card.Body>
+            </Card>
+            <Card className="card">
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="4">
+            <PaperCard title="Paper 1"/>
+            <PaperCard title="Paper 2"/>
+            <PaperCard title="Paper 3"/>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
