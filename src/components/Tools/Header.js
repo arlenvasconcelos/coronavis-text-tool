e5fc2a8df1c8192f1fced8f9cd840c7e9d4b90c0
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-05-13 17:45:01 -0300

    apllying material-ui on Header Component

--- src/components/Tools/Header.js
+++ src/components/Tools/Header.js
@@ -1,33 +1,57 @@
 import React from 'react';
-import {Col, Row} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import {Grid, Box, Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 
 import logoInf from '../../assets/images/inf-logo-2.png';
 import logoUfrgs from '../../assets/images/ufrgs-logo-2.png';
 
+const useStyle = makeStyles((theme) => ({
+  title: {
+    fontWeight: theme.typography.fontWeightBold,
+    '& *': {
+      color: theme.palette.common.black,
+    },
+    '& *:hover': {
+      textDecoration: 'none',
+      color: theme.palette.common.black,
+    }
+  },
+  image: {
+    maxHeight: '90px',
+  }
+}))
+
 export default function Header(){
+
+  const classes = useStyle();
 
   return (
     <>
-    <Row className="header my-2">
-      <Col 
-        md={4}
-        sm={6} 
-        className="d-flex justify-content-end align-items-center order-1 order-md-1"
-      >
-        <img className="header__img" alt="Logo UFRGS" src={logoUfrgs}/>
-      </Col>
-      <Col md={4} sm={12} className="text-center order-3 order-md-2">
-        <a className="header__link" href="/"><h1>COVID-19 Analysis Tools</h1></a>
-        <h3>Text Analytics for the COVID-19 Literature</h3>
-      </Col>
-      <Col 
-        md={4}
-        sm={6} 
-        className="d-flex justify-content-start align-items-center order-2 order-md-3"
-      >
-        <img className="header__img" alt="Logo INF" src={logoInf}/>
-      </Col>
-    </Row>
+    <Box display="flex" justifyContent="center">
+      <Box>
+        <img className={classes.image} alt="Logo UFRGS" src={logoUfrgs}/>
+      </Box>
+      <Box display="flex" alignItems="center" p={1}>
+        <Box>
+          <Typography 
+            component="h1"
+            variant="h6"
+            align="center"
+            className={classes.title}
+          >
+            {/* <a className="header__link" href="/"><h1>COVID-19 Analysis Tools</h1></a> */}
+            <Link to="/">COVID-19 Analysis Tools</Link>
+          </Typography>
+          <Typography component="h4" variant="subtitle1" align="center">
+            Text Analytics for the COVID-19 Literature
+          </Typography>
+        </Box>
+      </Box>
+      <Box>
+        <img className={classes.image} alt="Logo INF" src={logoInf}/>
+      </Box>
+    </Box>
     </>
   )
 }
--- src/template/Tools.js
+++ src/template/Tools.js
@@ -1,33 +1,15 @@
 import React from 'react';
 
-import {Container} from 'react-bootstrap';
+import {Container} from '@material-ui/core';
 import Header from '../components/Tools/Header';
 import NavSearch from '../components/Tools/NavSearch';
-// import routes from '../routes/routes'
-
-// const switchRoutes = (
-//   <Switch>
-//     {
-//       routes.map((prop, key) => {
-//         return (
-//           <Route
-//             path={"/tools" + prop.path}
-//             component={prop.component}
-//             key={key}
-//           />
-//         );
-//       })
-//     }
-//     <Redirect from="/tools" to="/tools/home" />
-//   </Switch>
-// );
 
 export default function Tools(props) { 
   return (
-    <div className="home">
+    <div>
       <Header/>
       <NavSearch/>
-      <Container fluid className="px-5">
+      <Container>
         {props.children}
       </Container>
       {/* <Footer/> */}
