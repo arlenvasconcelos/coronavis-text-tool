a86aac8f97460d118020a327ba7a4554515df0c5
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-05-18 23:43:07 -0300

    remove mainTopic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 
 import Searched from '../components/Home/Searched';
-import MainTopic from '../components/Home/MainTopic';
 import Topics from '../components/Home/Topics';
 
 export default function Home(){
@@ -14,7 +13,6 @@ export default function Home(){
       {dataSearched.papers 
         ? <Searched dataSearched={dataSearched}/> 
         : <>
-            <MainTopic />
             <Topics />
           </>
       }
