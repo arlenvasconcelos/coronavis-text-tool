fbda695fa497d4f0eb7bffb204819bc7f89eeba5
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-05-20 22:56:01 -0300

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+
 
 //components
 import Home from './pages/Home';
@@ -8,6 +12,17 @@ import Answers from './pages/Answers';
 import Tools from './template/Tools.js' 
 //Store
 import store from './store';
+
+const history = createBrowserHistory();
+
+const trackingId = "UA-167291036-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 
 const ToolsRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -24,7 +39,7 @@ export default function App() {
   return (
     <div>
       <Provider store={store}>
-        <Router>
+        <Router history={history}>
           <Switch>
             <ToolsRoute path="/tools/home" exact component={Home} />
             <ToolsRoute path="/tools/questions/:id/answers" exact component={Answers} />
