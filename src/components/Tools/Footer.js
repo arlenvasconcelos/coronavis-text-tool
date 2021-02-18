7644c9304afe1b0d35967351ba28aa2c949cb0b2
abel <abel@cpd.ufrgs.br>
2021-02-18 12:56:19 -0300

    - Correção do warning no footer para recuperar a release date. - Atualização de dependências no package.json e inclusão do package-lock.json para fazer o build com npm ci.

--- src/components/Tools/Footer.js
+++ src/components/Tools/Footer.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@material-ui/core";
+//import api
+import api from "../../service/api";
+
+
 
 export default function Footer() {
+
+  const [releaseDate, setReleaseDate] = useState([]);
+  
+
+  useEffect(() => {
+    const getReleaseDate = async () => {
+      try {
+        const response = await api.get(`/dataset`);
+        setReleaseDate(response.data.data.date);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getReleaseDate();
+  }, []);
+
+
   return (
     <footer>
       <Box textAlign="center" paddingY={3}>
         <Typography component="p" variant="subtitle2">
           Source:
           <a href="https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases.html">
-            CORD-19 Dataset | Full-Text Anserini Pre-Built Index: 2021-01-26
+            CORD-19 Dataset | Full-Text Anserini Pre-Built Index: {releaseDate}
           </a>
         </Typography>
       </Box>
